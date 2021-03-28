353359520adef1ef82d5d8bf91613c4332755ea1
IanAragonSaenz <a01177009@itesm.mx>
2021-03-28 14:55:47 -0400

    starting point, .h and .c created

---
+++ lib/src/data.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "data.h"
+
+void readInfo(Alumno &alumno, char *line){
+
+}
+
+void readGrades(Alumno &alumno, char *line){
+
+}
---
+++ lib/src/data.h
@@ -0,0 +1,20 @@
+#ifndef _DATA_H
+#define _DATA_H
+
+typedef struct {
+	int id;
+	char *nombre;
+	char *carrera;
+	char *ciudad;
+	char *graduacion;
+	int A;
+	int B;
+	int C;
+	int D;
+} Alumno;
+
+void readInfo(Alumno &alumno, char *line);
+
+void readGrades(Alumno &alumno, char *line);
+
+#endif
