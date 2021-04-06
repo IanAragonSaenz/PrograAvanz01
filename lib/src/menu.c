564a8ff9018bedc100f3869fae61e99ab9cdabfe
IanAragonSaenz <a01177009@itesm.mx>
2021-04-06 14:55:42 -0400

    added option to menu

--- lib/src/menu.c
+++ lib/src/menu.c
@@ -3,6 +3,7 @@
 void printMenu(){
     printf("Elija una opcion: \n");
     printf("1. Mostrar los estudiantes\n");
-    printf("2. mostrar toda la informacion de todos los estudiantes\n");
-    printf("3. hacer una consulta\n");
+    printf("2. Mostrar toda la informacion de todos los estudiantes\n");
+    printf("3. Mostrar todas las calificaciones de los estudiantes\n");
+    printf("4. Hacer una consulta\n");
 }
